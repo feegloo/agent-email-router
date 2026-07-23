9cb718340cf01eb9351f3389ca57e8d3f6bf3e6e
Aleksander Figiel <olek.figiel@gmail.com>
2026-07-23 12:38:20 +0200

    feat: pydantic-settings, environment variables

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    smpt_host: str
+    smpt_port: int
+    email_from: str
+    email_human_resources: str
+    email_help_desk: str
+    email_it: str
+    email_kadry: str
+    email_other: str
+    ollama_url: str
+    ollama_model: str
+
+settings = Settings() # type: ignore[call-arg]
